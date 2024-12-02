1c47f0ec8dd9084f897cbccfcc0c95ca8d5d23c9
jean-baptiste <jeanbaptiste.marrec@gmail.com>
2024-12-02 19:14:17 +0100

    add -date order by

--- my_project/posts/views.py
+++ my_project/posts/views.py
@@ -6,6 +6,6 @@ from .models import Post
 
 
 def posts_list(request):
-    posts = Post.objects.all().order_by('date')                                                                                                                                                                                                                         
+    posts = Post.objects.all().order_by('-date')                                                                                                                                                                                                                         
     return render(request, 'posts/posts_list.html' , {'posts':posts})
 
