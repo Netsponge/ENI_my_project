e7158d030a467034e7b30eb3dfc9d75d13cc969a
jean-baptiste <jeanbaptiste.marrec@gmail.com>
2024-12-13 15:05:34 +0100

    delete my proj

--- create_django_project.py
+++ create_django_project.py
@@ -1,11 +1,8 @@
 import os
 import subprocess
 import sys
-from colorama import Fore, init
-init()
-import django
-from django.contrib.auth import get_user_model
-from django.core.management import call_command
+
+
 
 # Project name and directory paths
 PROJECT_NAME = "my_project"
